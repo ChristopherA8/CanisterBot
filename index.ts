77ba22dd62b7427b74613bdd11da931960fd4967
Chr1sDev <christopheranderson3@me.com>
2021-05-28 07:09:27 +0000

    add slash commands

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client, Intents } = require("discord.js");
+const client = new Client({ intents: [Intents.NON_PRIVILEGED] });
 const { token } = require("./config.json");
 const axios = require("axios");
 
@@ -7,7 +8,7 @@ client.on(`ready`, () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on(`message`, (msg) => {
+client.on(`message`, async (msg) => {
   if (msg.author.bot) return;
   let currentPage = 0;
 
@@ -25,6 +26,8 @@ client.on(`message`, (msg) => {
       .then(function (res) {
         let response = res.data;
         const package = response.data[0];
+
+        // This is uhhh...... :/
         if (!package) throw new Error("Package not found");
 
         // Why why why
@@ -198,6 +201,79 @@ client.on(`message`, (msg) => {
         msg.channel.send(embed);
       });
   }
+});
+
+client.on("interaction", (interaction) => {
+  if (!interaction.isCommand()) return;
+  if (interaction.commandName !== "search") return;
+  let packageName = interaction.options[0].value;
+
+  axios
+    .get(
+      `https://api.canister.me/v1/community/packages/search?query=${packageName}&searchFields=packageId,name&responseFields=packageId,name,price,description,icon,repositoryURI,author,latestVersion,nativeDepiction,depiction,versions`
+    )
+    .then(function (res) {
+      let response = res.data;
+      const package = response.data[0];
+
+      // pls change this :)
+      if (!package) throw new Error("Package not found");
+
+      // Why why why
+      let icon = package.icon;
+      if (icon) {
+        if (package.icon.includes(`file://`)) icon = "";
+      }
+
+      const embed = new Discord.MessageEmbed()
+        .setColor("#0099ff")
+        .setAuthor(
+          `${package.name}`,
+          ``,
+          `${package.depiction ? package.depiction : ""}`
+        )
+        .setThumbnail(icon ? icon : ``)
+        .setDescription(
+          package.description ? package.description : "No Description"
+        )
+        .addFields(
+          {
+            name: "Author",
+            value: package.author ? package.author : `No Author`,
+            inline: true,
+          },
+          {
+            name: "Version",
+            value: package.latestVersion ? package.latestVersion : `No Version`,
+            inline: true,
+          },
+          {
+            name: "Price",
+            value: package.price ? package.price : `No Price`,
+            inline: true,
+          },
+          {
+            name: "BundleID",
+            value: package.packageId ? package.packageId : `No BundleID`,
+            inline: false,
+          },
+          {
+            name: "Depiction",
+            value: package.depiction ? package.depiction : `No Depiction`,
+            inline: false,
+          }
+        )
+        .setFooter(response.date, icon ? icon : ``);
+
+      interaction.reply(embed).catch((err) => {
+        console.log(err);
+        interaction.reply(`Something went wrong`, { ephemeral: true });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      interaction.reply(`Something went wrong`, { ephemeral: true });
+    });
 });
 
 client.login(token);
